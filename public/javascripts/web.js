4c573183207062f287b3eb655756bd917f9f0687
Amanda Lee <alee1@alee1-Latitude-e6420.(none)>
2013-03-05 05:06:30 -0500

    amanda's additions

--- public/javascripts/web.js
+++ public/javascripts/web.js
@@ -1,39 +1,86 @@
-  $(function(){console.log('This is something')})
+$(function () {
+  $('#composeTweetForm').click('submit', function () {
+  console.log('working')
+  })
   
-$('#composeTweetForm').click('submit', function () {
-console.log('working')
+  var url = $('#audioSummary').text();
+  console.log(url);
+  $.get(url, function(data) {
+    console.log(data);
+  });
+
+  window.onload = init;
+  var context;
+  var bufferLoader;
+  var RhythmSample = {
+  };
+  function init() {
+      context = new webkitAudioContext();
+      bufferLoader = new BufferLoader(
+            context,
+            [
+                  '../sounds/hihat.mp3',
+                  '../sounds/snare.mp3',
+                  '../sounds/kick.mp3',
+                  '../sounds/mansion.mp3'
+             ],
+             finishedLoading
+            );
+      bufferLoader.load();
+  }
+
+
+
+  function finishedLoading(bufferList) {
+      var source1 = context.createBufferSource();
+      source1.buffer = bufferList[0];
+      source1.connect(context.destination);
+      var source2 = context.createBufferSource();
+      source2.buffer = bufferList[1];
+      source2.connect(context.destination);
+      var source3 = context.createBufferSource();
+      source3.buffer = bufferList[2];
+      source3.connect(context.destination);
+      var source4 = context.createBufferSource();
+      source4.buffer = bufferList[3];
+      source4.connect(context.destination);
+      RhythmSample.play(bufferList);
+  }
+
+  RhythmSample.play = function(bufferList) {
+    function playSound(buffer, time) {
+      var source = context.createBufferSource();
+      source.buffer = buffer;
+      source.connect(context.destination);
+      source.noteOn(time);
+    }
+
+    var kick = bufferList[2];
+    var snare = bufferList[1];
+    var hihat = bufferList[0];
+    var song = bufferList[3];
+
+    // We'll start playing the rhythm 100 milliseconds from "now"
+    var startTime = 0.100;
+    var tempo = 87.804; // BPM (beats per minute)
+    var eighthNoteTime = (60 / tempo) / 2;
+    playSound(song,0);
+    // Play 2 bars of the following:
+    for (var bar = 0; bar < 200; bar++) {
+      var time = startTime + bar * 8 * eighthNoteTime;
+      // Play the bass (kick) drum on beats 1, 5
+      playSound(kick, time);
+      playSound(kick, time + 4 * eighthNoteTime);
+
+      // Play the snare drum on beats 3, 7
+      playSound(snare, time + 2 * eighthNoteTime);
+      playSound(snare, time + 6 * eighthNoteTime);
+
+      // Play the hi-hat every eighthh note.
+      for (var i = 0; i < 8; ++i) {
+//        playSound(hihat, time + i * eighthNoteTime);
+      }
+    }
+  };
+
 })
-
-$('#play').on('submit', function () {
-           console.log('hey')
-           window.onload = init;
-           var context;
-           var bufferLoader;
-    
-           function init() {
-                 context = new webkitAudioContext();
-         
-                 bufferLoader = new BufferLoader(
-                       context,
-                       [
-                             '../Candleinthesun.mp3',
-                             '../ForrestGump.mp3'
-                       ],
-                       finishedLoading
-                       );
-           bufferLoader.load();
-           }
-     
-           function finishedLoading(bufferList) {
-                 var source1 = context.createBufferSource();
-                 var source2 = context.createBufferSource();
-                 source1.buffer = bufferList[0];
-                 source2.buffer = bufferList[1];
-
-                 source1.connect(context.destination);
-                 source2.connect(context.destination);
-                 source1.noteOn(0);
-                 source2.noteOn(0);
-           }
-})
-
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index',{title:'yo'})
-  console.log('There are things appearing')
+var echojs = require('echojs');
+
+var echo = echojs({
+  key: "XCVC9ZYTCHJC5TMUI"
+});
+
+	exports.index = function(req, res){
+	echo('song/search').get({
+	  artist: '2pac',
+	  title: "thugz mansion",
+	  bucket: 'audio_summary'
+	}, function (err, json) {
+	  var url = json.response.songs[0].audio_summary.analysis_url;
+	  res.render('index', { title: 'yo' , audioUrl: url});
+	});
+	console.log('There are things appearing')
  };
