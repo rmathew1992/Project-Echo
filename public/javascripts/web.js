8a926974571f72b986a14bc0f42b14f3ed06cb30
Rachel Mathew <rachelmathew999@gmail.com>
2013-03-05 03:08:03 -0500

    2 songs at once

--- public/javascripts/web.js
+++ public/javascripts/web.js
@@ -3,12 +3,15 @@ $(function(){
       window.onload = init;
       var context;
       var bufferLoader;
+     
       function init() {
             context = new webkitAudioContext();
+           
             bufferLoader = new BufferLoader(
                   context,
                   [
-                        '../Candleinthesun.mp3'
+                        '../Candleinthesun.mp3',
+                        '../ForrestGump.mp3'
                    ],
                   finishedLoading
                   );
@@ -16,8 +19,13 @@ $(function(){
       }
       function finishedLoading(bufferList) {
             var source1 = context.createBufferSource();
+            var source2 = context.createBufferSource();
             source1.buffer = bufferList[0];
+            source2.buffer = bufferList[1];
+
             source1.connect(context.destination);
+            source2.connect(context.destination);
             source1.noteOn(0);
+            source2.noteOn(0);
       }
 })
