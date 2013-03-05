b4b6e5ee083b3a472aec385b39ad43196e091d43
Rachel Mathew <rachelmathew999@gmail.com>
2013-03-05 04:51:04 -0500

    submit button

--- public/javascripts/web.js
+++ public/javascripts/web.js
@@ -1,31 +1,39 @@
-$(('play').click(function(){
-      console.log('hey')
-      window.onload = init;
-      var context;
-      var bufferLoader;
-     
-      function init() {
-            context = new webkitAudioContext();
-           
-            bufferLoader = new BufferLoader(
-                  context,
-                  [
-                        '../Candleinthesun.mp3',
-                        '../ForrestGump.mp3'
-                   ],
-                  finishedLoading
-                  );
-            bufferLoader.load();
-      }
-      function finishedLoading(bufferList) {
-            var source1 = context.createBufferSource();
-            var source2 = context.createBufferSource();
-            source1.buffer = bufferList[0];
-            source2.buffer = bufferList[1];
-
-            source1.connect(context.destination);
-            source2.connect(context.destination);
-            source1.noteOn(0);
-            source2.noteOn(0);
-      }
+  $(function(){console.log('This is something')})
+  
+$('#composeTweetForm').click('submit', function () {
+console.log('working')
 })
+
+$('#play').on('submit', function () {
+           console.log('hey')
+           window.onload = init;
+           var context;
+           var bufferLoader;
+    
+           function init() {
+                 context = new webkitAudioContext();
+         
+                 bufferLoader = new BufferLoader(
+                       context,
+                       [
+                             '../Candleinthesun.mp3',
+                             '../ForrestGump.mp3'
+                       ],
+                       finishedLoading
+                       );
+           bufferLoader.load();
+           }
+     
+           function finishedLoading(bufferList) {
+                 var source1 = context.createBufferSource();
+                 var source2 = context.createBufferSource();
+                 source1.buffer = bufferList[0];
+                 source2.buffer = bufferList[1];
+
+                 source1.connect(context.destination);
+                 source2.connect(context.destination);
+                 source1.noteOn(0);
+                 source2.noteOn(0);
+           }
+})
+
