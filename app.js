bfa9bb88e988a964a7c4180b823b58201d1c59bc
Rachel Mathew <rachelmathew999@gmail.com>
2013-03-05 06:25:54 -0500

    Search function

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/search', routes.search);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,33 @@
 /*
  * GET home page.
  */
-
 var echojs = require('echojs');
 
 var echo = echojs({
   key: "XCVC9ZYTCHJC5TMUI"
 });
 
-	exports.index = function(req, res){
+exports.index = function(req, res){
+	//Finding information about original
+	echo('song/search').get({  
+		artist:'2pac',
+		title:'thugz mansion',
+		bucket:'audio_summary',
+	},function(err,json){
+		var url = json.response.songs[0].audio_summary.analysis_url;
+		res.render('index', { title: 'yo' , audioUrl: url});
+	}) 
+}  
+
+exports.search=function(req,res){
+	//Related song search
 	echo('song/search').get({
-	  artist: '2pac',
-	  title: "thugz mansion",
-	  bucket: 'audio_summary'
+		key:8,
+		max_tempo:87.804,
+		style:'classical',
+		bucket: 'artist',
+		bucket:['tracks', 'id:rdio-US']
 	}, function (err, json) {
-	  var url = json.response.songs[0].audio_summary.analysis_url;
-	  res.render('index', { title: 'yo' , audioUrl: url});
-	});
-	console.log('There are things appearing')
- };
+		res.send(json.response.songs)
+	})
+}
